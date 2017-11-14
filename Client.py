68ca43585db3536a11b40c38275f21262c7aa70e
feliperonderos <feliperonderos@gmail.com>
2017-11-13 22:04:57 -0600

    n

--- Client.py
+++ Client.py
@@ -1,12 +1,12 @@
 from socket import *
 import sys
-f = open("cl.txt","w+")
-f.write("hi")
-f.close()
 if (len(sys.argv) == 2):
   while True:
   	s = socket(AF_INET,SOCK_STREAM)
   	s.connect((sys.argv[1],12001))
   	s.send("poop".encode())
   	reply = s.recv(1024)
+  	f = open("cl.txt","w+")
+  	f.write(reply)
+  	f.close()
   	s.close()
