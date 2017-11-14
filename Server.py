685a7e1b151e1e54c7d8375a336681a5ab79cd1b
feliperonderos <feliperonderos@gmail.com>
2017-11-13 21:39:21 -0600

    n

--- Server.py
+++ Server.py
@@ -2,13 +2,15 @@ from socket import *
 port = 12001
 s = socket(AF_INET,SOCK_STREAM)
 s.bind(("",port))
-f= open("guru99.txt","w+")
-f.write("poop")
-f.close()
 s.listen(1)
+f= open("guru99.txt","w+")
+f.write("pppppaap")
+f.close()
 while True:
   connectionSock, addr = s.accept()
-  print "New connection"
+  f= open("guru99.txt","w+")
+  
+  f.close()
   sentence = connectionSock.recv(1024).decode()
   cap = sentence.upper()
   connectionSock.send(cap.encode())
