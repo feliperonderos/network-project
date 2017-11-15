15cdaabe5eea66f13674ee383847a3865436e100
feliperonderos <feliperonderos@gmail.com>
2017-11-14 20:10:59 -0600

    n

--- Client.py
+++ Client.py
@@ -1,11 +1,12 @@
 from socket import *
+import random, string
 import sys
 if (len(sys.argv) == 3):
   while True:
+  	p ="".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2000))
+  	print p
   	s = socket(AF_INET,SOCK_STREAM)
   	s.connect((sys.argv[1],12001))
-  	p ="".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2000))
   	while True:
   		s.send(p.encode())
-  		s.recv(1024)
   	s.close()
--- Server.py
+++ Server.py
@@ -1,15 +1,16 @@
 from socket import *
+messages = {}
 port = 12001
 s = socket(AF_INET,SOCK_STREAM)
 s.bind(("",port))
 s.listen(5)
 while True:
   connectionSock, addr = s.accept()
-  sentence = ""
-  print 'run'
-  while sentence != "END":
-  	sentence = connectionSock.recv(1024).decode()
-  	cap = sentence.upper()
-  	print cap
-  	connectionSock.send(cap.encode())
+  msg = ""
+  while msg != "END":
+  	msg = connectionSock.recv(1024).decode()
+  	if addr in messages:
+  		messages[addr].append(msg)
+  	else:
+  		messages[addr] = [msg]
   connectionSock.close()
