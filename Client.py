80c5525ada994ee470da782c271523b83b7f9bb8
feliperonderos <feliperonderos@gmail.com>
2017-11-14 20:36:16 -0600

    n

--- Client.py
+++ Client.py
@@ -1,12 +1,15 @@
 from socket import *
+import time
 import random, string
 import sys
 if (len(sys.argv) == 3):
   while True:
-  	p ="".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2000))
-  	print p
-  	s = socket(AF_INET,SOCK_STREAM)
-  	s.connect((sys.argv[1],12001))
-  	while True:
-  		s.send(p.encode())
-  	s.close()
+    try:
+	    p ="".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2000))
+	    #p = sys.argv[2]
+	    s = socket(AF_INET,SOCK_STREAM)
+	    s.connect((sys.argv[1],12001))
+	    while True:
+	    	s.send(p.encode())
+    except:
+	  	s.close()
--- Server.py
+++ Server.py
@@ -1,16 +1,52 @@
+"""
 from socket import *
 messages = {}
 port = 12001
 s = socket(AF_INET,SOCK_STREAM)
-s.bind(("",port))
+s.bind(("localhost",port))
 s.listen(5)
 while True:
   connectionSock, addr = s.accept()
   msg = ""
   while msg != "END":
   	msg = connectionSock.recv(1024).decode()
+  	print msg
   	if addr in messages:
   		messages[addr].append(msg)
   	else:
   		messages[addr] = [msg]
   connectionSock.close()
+
+"""
+
+
+import socket
+from threading import Thread
+from SocketServer import ThreadingMixIn
+ 
+class ClientThread(Thread):
+  def __init__(self,ip,port):
+	  Thread.__init__(self)
+	  self.ip = ip
+	  self.port = port
+  def run(self):
+    while True:
+    	data = conn.recv(2048)
+    	if not data: break
+    	print "received data:", data
+    
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("", 12001))
+threads = []
+ 
+while True:
+    s.listen(4)
+    print "Waiting for incoming connections..."
+    (conn, (ip,port)) = s.accept()
+    newthread = ClientThread(ip,port)
+    newthread.start()
+    threads.append(newthread)
+ 
+for t in threads:
+    t.join()
