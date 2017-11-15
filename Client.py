cb5ed77866c0e0edefd876f41722c43774931c07
feliperonderos <feliperonderos@gmail.com>
2017-11-14 19:54:52 -0600

    n

--- Client.py
+++ Client.py
@@ -4,9 +4,8 @@ if (len(sys.argv) == 3):
   while True:
   	s = socket(AF_INET,SOCK_STREAM)
   	s.connect((sys.argv[1],12001))
-  	p ="pooop"#''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2000))
+  	p ="".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2000))
   	while True:
   		s.send(p.encode())
-  		print "sent"
   		s.recv(1024)
   	s.close()
