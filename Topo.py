7e8195175759996e4113f50df2c1d436999536a0
feliperonderos <feliperonderos@gmail.com>
2017-11-17 02:17:29 -0600

    n

--- Topo.py
+++ Topo.py
@@ -32,12 +32,12 @@ class Topo(Topo):
             if h < 2:
                 host = self.addHost('h%s' % (h + 1))#, cpu=.5/n)
             else:
-                host = self.addHost('h%s' % (h + 1), cpu=.25/n)
+                host = self.addHost('h%s' % (h + 1), cpu=.5/n)
             if h == 0:
-                self.addLink(host, edge[h%len(edge)], bw=50, delay='5ms', loss=2,
+                self.addLink(host, edge[h%len(edge)], bw=25, delay='5ms', loss=2,
                           max_queue_size=1000, use_htb=True )
             else:
-                self.addLink(host, edge[h%len(edge)], bw=1, delay='5ms', loss=2,
+                self.addLink(host, edge[h%len(edge)], bw=2, delay='5ms', loss=2,
                           max_queue_size=1000, use_htb=True )
 
 def simpleTest(num_hosts):
